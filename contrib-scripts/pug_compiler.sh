e7685eb905cb91da34b516adcfd447b7cf72c31f
Lucas Fonseca dos Santos <lucas@lcfcompany.com.br>
2019-10-08 06:36:27 +0000

    :zap: Adding sub-login component at Components ITCSS style.

---
+++ contrib-scripts/pug_compiler.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo pug --watch ../front-end-src/ext/pug/*.pug -o ../front-end-src/ --style compressed
---
+++ contrib-scripts/sass_compiler.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo sass --watch ../front-end-src/ext/sass:../front-end-src/ext/css
